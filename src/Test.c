325a4ca5589910e894947993e02e35a2a105ee3d
Mustafa <mmermmus123@gmail.com>
2023-05-06 14:50:32 +0300

    example usage of char in generic arraylist

--- src/Test.c
+++ src/Test.c
@@ -6,12 +6,20 @@ int main()
 {
 
     ArrayList *list = createArrayList(10, INT);
+    ArrayList *clist = createArrayList(10, CHAR);
 
     // Append integer elements
     int a = 1, b = 2, c = 3;
+
+    // append char elements
+    char c_a = 'a', c_b = 'b', c_c = 'c';
     list->append(list, &a);
     list->append(list, &b);
     list->append(list, &c);
+
+    clist->append(clist, &c_a);
+    clist->append(clist, &c_b);
+    clist->append(clist, &c_c);
 
     // Get an element
     int *element = (int *)list->get(list, 2);
@@ -19,6 +27,9 @@ int main()
 
     // Display the list
     list->display(list);
+
+    clist->display(clist);
+    clist->destroyArrayList(clist);
 
     // Clean up
     list->destroyArrayList(list);
