87050b5b38292f53efd2edebfd2b1939160168c0
Mustafa <mmermmus123@gmail.com>
2023-05-08 19:55:25 +0300

    colony struct support added to data types

--- include/utils/DataType.h
+++ include/utils/DataType.h
@@ -1,10 +1,13 @@
 #if !defined(DATATYPE)
 #define DATATYPE
+#include "../colony/Colony.h"
 
+#define COLONY_SIZE sizeof(Colony)
 typedef enum
 {
     CHAR = 1,
     INT = 4,
+    COLONYY = COLONY_SIZE
 } DataType;
 
 #endif // DATATYPES
