0edb3df31993fd49eb944f1c9ae20aa972f4da6b
Mustafa <mmermmus123@gmail.com>
2023-05-11 01:31:15 +0300

    code cleaned

--- include/utils/DataType.h
+++ include/utils/DataType.h
@@ -4,20 +4,17 @@
 #include "Bool.h"
 #include "../log/Log.h"
 
-#define COLONY_SIZE sizeof(Colony)
-#define CHAR_SIZE sizeof(char)
-#define INT_SIZE sizeof(int)
-#define LOG_SIZE sizeof(Log)
-
 typedef enum
 {
-    CHAR = CHAR_SIZE,
-    INT = INT_SIZE,
-    _LOG = LOG_SIZE,
-    _ARRAYLIST = 64,
-    _COLONY = COLONY_SIZE,
-    BOOL = COLONY_SIZE + 1,
-    _STRING = BOOL + 1,
+    CHAR,
+    INT,
+    PTR,
+    _LOG,
+    _ARRAYLIST,
+    _COLONY,
+    BOOL,
+    _STRING,
+    _GAME
 } DataType;
 extern char *DataTypeStrings[];
 #endif // DATATYPES
