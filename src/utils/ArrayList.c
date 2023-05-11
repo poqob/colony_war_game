976553004939de69bdcce818a7768750b76d0488
Mustafa <mmermmus123@gmail.com>
2023-05-11 16:00:29 +0300

    update

--- src/utils/ArrayList.c
+++ src/utils/ArrayList.c
@@ -98,6 +98,35 @@ ArrayList *createArrayList(int initialCapacity, DataType type)
     this->dataType = type;
 
     this->array = malloc(initialCapacity * sizeof(this->dataType));
+
+    switch (this->dataType)
+    {
+    case CHAR:
+        this->array = (void *)malloc(initialCapacity * sizeof(char *));
+        break;
+    case INT:
+        this->array = (void *)malloc(initialCapacity * sizeof(int *));
+        break;
+    case PTR:
+        this->array = (void *)malloc(initialCapacity * sizeof(void *));
+        break;
+    case _ARRAYLIST:
+        this->array = (void *)malloc(initialCapacity * sizeof(ArrayList *));
+        break;
+    case _COLONY:
+        this->array = (void *)malloc(initialCapacity * sizeof(Colony *));
+        break;
+    case BOOL:
+        this->array = (void *)malloc(initialCapacity * sizeof(boolean *));
+        break;
+    case _STRING:
+        this->array = (void *)malloc(initialCapacity * sizeof(String *));
+        break;
+    default:
+        this->array = (void *)malloc(initialCapacity * sizeof(void *));
+        break;
+    }
+
     if (this->array == NULL)
     {
         free(this);
